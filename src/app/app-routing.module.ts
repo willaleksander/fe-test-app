f575e9f18fba03fef7487d44c397dee7cc538cca
Willyan Aleksander <willyan@whitefox.com.br>
2022-04-05 18:05:58 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { UsersComponent } from './users/users.component';
+import { UsersCreateComponent } from './users-create/users-create.component'; 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: UsersComponent },
+  { path: 'users/create', component: UsersCreateComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersComponent } from './users/users.component';
+import { UsersCreateComponent } from './users-create/users-create.component';
+
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UsersCreateComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
